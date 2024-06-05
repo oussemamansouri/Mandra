36072e0fa8ab870f2b42cecb266fa506d77bcfb6
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-05 23:05:01 +0100

    Create the backend for the project Mandra

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/MandraApplication.java
@@ -0,0 +1,13 @@
+package com.elife.mandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandraApplication.class, args);
+	}
+
+}
---
+++ BackEnd/mandra/src/test/java/com/elife/mandra/MandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elife.mandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
