63dc5cc56dcf00e112bf5dd7d7dfb18ba7805b91
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 22:15:06 +0100

    Create ClientController.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
@@ -0,0 +1,32 @@
+package com.elife.mandra.Web.RestControllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elife.mandra.Business.Services.ClientService;
+import com.elife.mandra.DAO.Entities.Client;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class ClientController {
+
+   
+ final ClientService clientService;
+ public ClientController(ClientService clientService){
+    this.clientService = clientService;
+ }   
+
+
+ @PostMapping("/clients/create")
+ public ResponseEntity<Object> createClient(@RequestBody Client client) {
+     return new ResponseEntity<>(this.clientService.addClient(client),HttpStatus.OK);
+ }
+ 
+
+
+
+}
