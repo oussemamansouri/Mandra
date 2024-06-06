e1781e2229c0eee02fb0a20ca7b97f772348e560
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 01:40:54 +0100

    Create Restaurant.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Restaurant.java
@@ -0,0 +1,24 @@
+package com.elife.mandra.DAO.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "Restaurant")
+public class Restaurant extends Property {
+    @Id
+    @GeneratedValue(  strategy = GenerationType.IDENTITY)
+    private long id ;
+}
