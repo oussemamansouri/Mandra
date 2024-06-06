895ce8f019a1046947df900efef74ccc3a897920
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 20:44:59 +0100

    Create ClientServiceImp.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/ClientServiceImp.java
@@ -0,0 +1,50 @@
+package com.elife.mandra.Business.ServicesImp;
+
+import com.elife.mandra.Business.Services.ClientService;
+import com.elife.mandra.DAO.Entities.Client;
+
+public class ClientServiceImp implements ClientService {
+
+    @Override
+    public Client addClient(Client client) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addClient'");
+    }
+
+    @Override
+    public Client getClients() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getClients'");
+    }
+
+    @Override
+    public Client getClientById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getClientById'");
+    }
+
+    @Override
+    public Client updateClient(Client client) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateClient'");
+    }
+
+    @Override
+    public Client updateClientImage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateClientImage'");
+    }
+
+    @Override
+    public Client updateClientPassword() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateClientPassword'");
+    }
+
+    @Override
+    public void deleteClient(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteClient'");
+    }
+
+}
