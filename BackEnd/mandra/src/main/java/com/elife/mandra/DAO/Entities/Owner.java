95b29525554fd8357861178966fd644800321f4b
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 01:22:46 +0100

    Delete the extra imports

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Owner.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Owner.java
@@ -1,6 +1,5 @@
 package com.elife.mandra.DAO.Entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
