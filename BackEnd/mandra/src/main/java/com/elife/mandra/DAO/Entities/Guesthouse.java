b21eef507f7a2dbe717dece873a0bbbada1fed7b
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 14:17:04 +0100

    Create Guesthouse.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Guesthouse.java
@@ -0,0 +1,28 @@
+package com.elife.mandra.DAO.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+public class Guesthouse extends Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hasPool",nullable = false)
+    private boolean aPiscine ;
+
+}
