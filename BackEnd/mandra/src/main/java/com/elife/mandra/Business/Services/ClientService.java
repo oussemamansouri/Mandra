fbe599931ab98d6d91dcf14188dc45cc61242fd6
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 20:44:10 +0100

    Create ClientService.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/ClientService.java
@@ -0,0 +1,22 @@
+package com.elife.mandra.Business.Services;
+
+import com.elife.mandra.DAO.Entities.Client;
+
+public interface ClientService {
+
+    //Create operation
+    public Client addClient(Client client);
+
+    //Read operation
+    public Client getClients();
+    public Client getClientById(Long id);
+ 
+    //Update operation
+    public Client updateClient(Client client);
+    public Client updateClientImage();
+    public Client updateClientPassword();
+
+    //Delete operation
+    public void deleteClient(Long id); 
+
+}
