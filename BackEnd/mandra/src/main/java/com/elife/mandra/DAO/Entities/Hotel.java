28379393c93ac4be6c98eb5e7fd2428065710a78
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 01:31:59 +0100

    add the property class and  hotel extend

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Hotel.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Hotel.java
@@ -1,6 +1,5 @@
 package com.elife.mandra.DAO.Entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,72 +18,12 @@ import lombok.Setter;
 @Entity
 @Table(name = "Hotel")
 
-public class Hotel {
+public class Hotel extends Property {
 
-        // @Column(name = "code",length = 255,nullable = true , unique = false, insertable=true,updatable = true  )
 
         @Id
         @GeneratedValue( strategy = GenerationType.IDENTITY)
         private Long id ;
     
-        @Column(name = "name",length = 40,nullable = false )
-        private String name ;
-
-        @Column(name = "description",nullable = false )
-        private String description ;
-    
-        @Column(name = "email",length = 40,nullable = false)
-        private String email ;
-    
-        @Column(name = "address",length = 100,nullable = false)
-        private String address ;
-    
-        @Column(name = "city",length = 30,nullable = false)
-        private String city ;
-    
-        @Column(name = "phoneNumber",length = 20,nullable = false)
-        private int phoneNumber ;
-    
-        @Column(name = "website",nullable = true)
-        private String website ;
-
-        @Column(name = "numberOfRooms",nullable = false)
-        private int numberOfRooms ;
-
-        @Column(name = "hasParking",nullable = false)
-        private boolean aparking ;
-
-        @Column(name = "hasWifi",nullable = false)
-        private boolean awifi ;
-
-        @Column(name = "hasGym",nullable = false)
-        private boolean aSalleDeSport ;
-
-        @Column(name = "hasPool",nullable = false)
-        private boolean aPiscine ;
-
-        @Column(name = "hasRestaurant",nullable = false)
-        private boolean aRestaurant ;
-
-        @Column(name = "allowsPets",nullable = false)
-        private boolean accpteAnimaux ;
-
-        @Column(name = "facebook",nullable = true)
-        private String facebook ;
-
-        @Column(name = "instagram",nullable = true)
-        private String instagram ;
-
-        @Column(name = "nbOfRaters",nullable = true)
-        private long nbOfRaters ;
-
-        @Column(name = "rateScore",nullable = true)
-        private long rateScore ;
-        
-        @Column( name = "nbOfStars" )
-        private long nbOfStars;
-
-        @Column(name = "image",nullable = true)
-        private String image ;
 
 }
---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Property.java
@@ -0,0 +1,79 @@
+package com.elife.mandra.DAO.Entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@MappedSuperclass // Use @MappedSuperclass for a base class without its own table
+public class Property {
+
+      @Column(name = "name",length = 40,nullable = false )
+        private String name ;
+
+        @Column(name = "description",nullable = false )
+        private String description ;
+    
+        @Column(name = "email",length = 40,nullable = false)
+        private String email ;
+    
+        @Column(name = "address",length = 100,nullable = false)
+        private String address ;
+    
+        @Column(name = "city",length = 30,nullable = false)
+        private String city ;
+    
+        @Column(name = "phoneNumber",length = 20,nullable = false)
+        private int phoneNumber ;
+    
+        @Column(name = "website",nullable = true)
+        private String website ;
+
+        @Column(name = "numberOfRooms",nullable = false)
+        private int numberOfRooms ;
+
+        @Column(name = "hasParking",nullable = false)
+        private boolean aparking ;
+
+        @Column(name = "hasWifi",nullable = false)
+        private boolean awifi ;
+
+        @Column(name = "hasGym",nullable = false)
+        private boolean aSalleDeSport ;
+
+        @Column(name = "hasPool",nullable = false)
+        private boolean aPiscine ;
+
+        @Column(name = "hasRestaurant",nullable = false)
+        private boolean aRestaurant ;
+
+        @Column(name = "allowsPets",nullable = false)
+        private boolean accpteAnimaux ;
+
+        @Column(name = "facebook",nullable = true)
+        private String facebook ;
+
+        @Column(name = "instagram",nullable = true)
+        private String instagram ;
+
+        @Column(name = "nbOfRaters",nullable = true)
+        private long nbOfRaters ;
+
+        @Column(name = "rateScore",nullable = true)
+        private long rateScore ;
+        
+        @Column( name = "nbOfStars" )
+        private long nbOfStars;
+
+        @Column(name = "image",nullable = true)
+        private String image ;
+
+}
