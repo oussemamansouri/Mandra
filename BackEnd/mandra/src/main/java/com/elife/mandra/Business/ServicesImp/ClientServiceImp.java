b8af9c8ab427a48035011487cfd8f851b9113891
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 20:58:15 +0100

    adding the addClient methode in the clientServiceImp

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/ClientServiceImp.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/ClientServiceImp.java
@@ -1,14 +1,32 @@
 package com.elife.mandra.Business.ServicesImp;
 
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.elife.mandra.Business.Services.ClientService;
 import com.elife.mandra.DAO.Entities.Client;
+import com.elife.mandra.DAO.Repositories.ClientRepository;
 
+@Service
 public class ClientServiceImp implements ClientService {
+
+    final ClientRepository clientRepository ;
+    public ClientServiceImp(ClientRepository clientRepository ){
+        this.clientRepository = clientRepository;
+    }
+
+    //or you can use the Field injection
+    // @Autowired
+    // ClientRepository clientRepository ;
 
     @Override
     public Client addClient(Client client) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addClient'");
+        try {
+            return clientRepository.save(client);
+        } catch (Exception e) {
+            // Log the exception and rethrow it or handle it accordingly
+            throw new RuntimeException("Failed to save client: " + e.getMessage(), e);
+        }
     }
 
     @Override
