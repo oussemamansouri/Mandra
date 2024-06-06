0426f6d9b288a12a22fbd5c1f20fbdea5461365a
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 15:50:46 +0100

    Add the rolation between the admin and the his tables

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Admin.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Admin.java
@@ -1,9 +1,13 @@
 package com.elife.mandra.DAO.Entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,6 +26,12 @@ public class Admin extends User {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id ;
+
+    @OneToMany(mappedBy = "AdminId", cascade = CascadeType.ALL )
+    private List<GastronomicSpecialties> GastronomicSpecialties ;
+
+    @OneToMany(mappedBy = "AdminId", cascade = CascadeType.ALL )
+    private List<SpecialtyWomen> SpecialtyWomens ;
 
 
 }
--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/GastronomicSpecialties.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/GastronomicSpecialties.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Temporal;
@@ -61,5 +63,9 @@ public class GastronomicSpecialties {
         protected void onUpdate() {
             updateDate = new Date();
         }
+
+        @ManyToOne()
+        @JoinColumn(name = "AdminId")
+        private Admin AdminId ;
 
 }
