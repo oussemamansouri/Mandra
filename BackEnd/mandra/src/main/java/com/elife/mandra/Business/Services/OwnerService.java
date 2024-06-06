677051bf4ee5fc2024bc1b06678f70a862c2b843
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-06 22:18:10 +0100

    Create OwnerService.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/OwnerService.java
@@ -0,0 +1,22 @@
+package com.elife.mandra.Business.Services;
+
+import com.elife.mandra.DAO.Entities.Owner;
+
+public interface OwnerService {
+
+    //Create operation
+    public Owner addOwner(Owner owner);
+
+    //Read operation
+    public Owner getOwners();
+    public Owner getOwnerById(Long id);
+ 
+    //Update operation
+    public Owner updateOwner(Owner owner);
+    public Owner updateOwnerImage();
+    public Owner updateOwnerPassword();
+
+    //Delete operation
+    public void deleteOwner(Long id); 
+
+}
