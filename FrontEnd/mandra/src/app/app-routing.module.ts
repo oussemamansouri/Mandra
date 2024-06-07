ef9ec7244b9a4b3ef98d2d174a654c941b32cfec
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-07 18:18:18 +0100

    Update app-routing.module.ts

--- FrontEnd/mandra/src/app/app-routing.module.ts
+++ FrontEnd/mandra/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayouteComponent } from './layoutes/admin-layoute/admin-layoute.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'admin',component:AdminLayouteComponent,children:[
+    {path:'',redirectTo:'dashboard',pathMatch:'full'},
+    {path:'dashboard',loadChildren:()=> import('./views/Admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
+    {path:'profile',loadChildren:()=> import('./views/Admin/profile/profile.module').then(m=>m.ProfileModule)},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
