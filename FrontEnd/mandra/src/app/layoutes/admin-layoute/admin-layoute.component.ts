262f51d56bc05042163906991e0876aa3648d8ba
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-07 18:16:12 +0100

    Create admin-layoute.component.ts

---
+++ FrontEnd/mandra/src/app/layoutes/admin-layoute/admin-layoute.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layoute',
+  templateUrl: './admin-layoute.component.html',
+  styleUrls: ['./admin-layoute.component.css']
+})
+export class AdminLayouteComponent {
+
+}
