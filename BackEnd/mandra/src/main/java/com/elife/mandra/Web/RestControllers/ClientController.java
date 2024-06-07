69f07c5c08e6bce8119aa2721b25a9d5f08f5b29
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-07 16:50:13 +0100

    Update ClientController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-
-
 @RestController
 public class ClientController {
 
