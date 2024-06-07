3059dd961fe037be1ee10308fdcdd1e5a02fc277
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-07 18:16:58 +0100

    Update app.module.ts

--- FrontEnd/mandra/src/app/app.module.ts
+++ FrontEnd/mandra/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutesModule } from './layoutes/layoutes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
