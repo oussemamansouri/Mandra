a69746063127f42bfd232b83b9f79c1b8f713565
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-07 18:17:44 +0100

    Create layoutes.module.ts

---
+++ FrontEnd/mandra/src/app/layoutes/layoutes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminLayouteComponent } from './admin-layoute/admin-layoute.component';
+import { ClientLayouteComponent } from './client-layoute/client-layoute.component';
+import { OwnerLayouteComponent } from './owner-layoute/owner-layoute.component';
+import { FrontLayouteComponent } from './front-layoute/front-layoute.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    AdminLayouteComponent,
+    ClientLayouteComponent,
+    OwnerLayouteComponent,
+    FrontLayouteComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class LayoutesModule { }
