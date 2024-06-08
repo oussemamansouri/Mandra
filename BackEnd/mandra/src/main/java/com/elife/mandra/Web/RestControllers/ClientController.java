b02a9f6330bf86558c8e5f04086e79cd3db43bbf
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-08 01:48:05 +0100

    Update ClientController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
@@ -9,12 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
    
@@ -23,25 +25,30 @@ public class ClientController {
     this.clientService = clientService;
  }   
 
+ @PostMapping("/register")
+ public Client registerClient(@RequestBody Client client) {
+     return clientService.registerClient(client);
+ }
 
- @PostMapping("/clients/create")
+
+ @PostMapping("/create")
  public ResponseEntity<Object> createClient(@RequestBody Client client) {
      return new ResponseEntity<>(this.clientService.addClient(client),HttpStatus.OK);
  }
 
 
- @GetMapping("/clients")
+ @GetMapping("")
  public ResponseEntity<Object> getClients() {
      return new ResponseEntity<>(this.clientService.getClients(),HttpStatus.OK);
  }
  
- @GetMapping("/clients/{id}")
+ @GetMapping("/{id}")
  public ResponseEntity<Object> getClients( @PathVariable(value = "id") Long id  ) {
      return new ResponseEntity<>(this.clientService.getClientById(id),HttpStatus.OK);
  }
  
 
- @PatchMapping("/clients/{id}/delete")
+ @PatchMapping("/{id}/delete")
  public ResponseEntity<Object> putMethodName(@PathVariable Long id) {
     return new ResponseEntity<>(this.clientService.deleteClient(id),HttpStatus.OK);
 }
