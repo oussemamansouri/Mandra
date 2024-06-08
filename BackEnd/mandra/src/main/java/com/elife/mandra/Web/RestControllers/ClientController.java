774fffff007f91b433a5f8ceaaf2018ca7f25ffe
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-08 18:02:50 +0100

    Update ClientController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
@@ -10,8 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
@@ -48,7 +48,7 @@ public class ClientController {
  }
  
 
- @PatchMapping("/{id}/delete")
+ @DeleteMapping("/{id}/delete")
  public ResponseEntity<Object> putMethodName(@PathVariable Long id) {
     return new ResponseEntity<>(this.clientService.deleteClient(id),HttpStatus.OK);
 }
