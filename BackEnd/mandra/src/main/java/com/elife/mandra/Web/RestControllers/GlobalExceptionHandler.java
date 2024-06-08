626851c40ca38ba763d37226b59ff11d3145f195
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-08 19:38:23 +0100

    Update GlobalExceptionHandler.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/GlobalExceptionHandler.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.elife.mandra.Web.RestControllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +15,18 @@ import com.elife.mandra.Web.Responses.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    
       // Handle all other exceptions
       @ExceptionHandler(Exception.class)
       public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
