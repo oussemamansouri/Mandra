4c7496604b48f6bdc63b51fc2493186b34020136
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-09 02:40:47 +0100

    Update ClientController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ClientController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.elife.mandra.Business.Services.ClientService;
 import com.elife.mandra.DAO.Entities.Client;
+import com.elife.mandra.Web.Requests.ClientForms.RegisterClientForm;
 import com.elife.mandra.Web.Requests.ClientForms.UpdateClientForm;
 import com.elife.mandra.Web.Responses.ErrorResponse;
 
@@ -35,7 +36,7 @@ public class ClientController {
 
 
 @PostMapping("/register")
-    public ResponseEntity<?> registerClient(@Valid @RequestBody Client client, BindingResult result) {
+    public ResponseEntity<?> registerClient(@Valid @RequestBody RegisterClientForm client, BindingResult result) {
         if (result.hasErrors()) {
             StringBuilder errors = new StringBuilder();
             result.getAllErrors().forEach(error -> errors.append(error.getDefaultMessage()).append("; "));
