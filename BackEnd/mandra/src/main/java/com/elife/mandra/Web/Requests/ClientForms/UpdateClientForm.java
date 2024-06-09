fc8ec13919102d2d5361bd369d5778fa3362e3d7
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-09 02:00:13 +0100

    Create UpdateClientForm.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/Requests/ClientForms/UpdateClientForm.java
@@ -0,0 +1,30 @@
+package com.elife.mandra.Web.Requests.ClientForms;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class UpdateClientForm {
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Size(max = 20, message = "Le prénom doit comporter moins de 20 caractères")
+    private String firstname ;
+
+    @NotBlank(message = "Le nom de famille est obligatoire")
+    @Size(max = 20, message = "Le nom de famille doit comporter moins de 20 caractères.")
+    private String lastname ;
+
+    @NotNull(message = "Le numéro de téléphone est obligatoire")
+    private int phoneNumber ;
+
+}
