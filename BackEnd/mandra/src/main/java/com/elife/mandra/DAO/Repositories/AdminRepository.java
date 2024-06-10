ae3311c5cd6d5dafee29e5ca1d3b7db0ccf71b1e
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-10 17:13:25 +0100

    Update AdminRepository.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/AdminRepository.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/AdminRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.elife.mandra.DAO.Entities.Admin;
+import java.util.List;
+
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin,Long> {
+
+      public List<Admin> findByEmail(String email);
 
 }
