b29e3069bf977b34ddfd8859e2952c78facb1039
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-10 20:12:36 +0100

    update OwnerRepository

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/OwnerRepository.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/OwnerRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.elife.mandra.DAO.Entities.Owner;
+import java.util.List;
+
 
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner,Long>{
+
+    public List<Owner> findByEmail(String email);
 
 }
