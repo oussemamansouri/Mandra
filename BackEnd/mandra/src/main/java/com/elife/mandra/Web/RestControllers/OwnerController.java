f8eedb18dd1f1250e4c46772690564607c341523
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-12 14:41:39 +0100

    Update OwnerController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/OwnerController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/OwnerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +55,17 @@ public class OwnerController {
        Page<Owner> owner = ownerService.getOwners(pageable);
       return ResponseEntity.status(HttpStatus.OK).body(owner);
     }
+
+
+
+
+     // ----------------------------------      get Owner By Id endpoint     -----------------------------------
+
+ @GetMapping("/{id}")
+ public ResponseEntity<Object> getOwnerById( @PathVariable(value = "id") Long id  ) {
+    Owner owner= ownerService.getOwnerById(id);
+    return ResponseEntity.status(HttpStatus.OK).body(owner);
+ }
+ 
 
 }
