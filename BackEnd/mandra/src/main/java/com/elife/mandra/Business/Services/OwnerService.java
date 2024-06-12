5844885a7fc5dc220249c631a31511ae6ae5f97e
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-12 14:37:14 +0100

    Update OwnerService.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/OwnerService.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/OwnerService.java
@@ -1,8 +1,13 @@
 package com.elife.mandra.Business.Services;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.elife.mandra.DAO.Entities.Owner;
 import com.elife.mandra.Web.Requests.OwnerForms.AddOwnerForm;
+import com.elife.mandra.Web.Requests.UserForms.UpdatePasswordForm;
 
 public interface OwnerService {
 
@@ -10,13 +15,15 @@ public interface OwnerService {
     public Owner registerOwner(AddOwnerForm ownerForm);
 
     //Read operation
-    public Owner getOwners();
+    public Page<Owner> getOwners(Pageable pageable);
     public Owner getOwnerById(Long id);
  
     //Update operation
+    public Owner uploadCinImage(Long id, MultipartFile cinImage);
+    public Owner uploadProofFile(Long id, MultipartFile proofFile);
     public Owner updateOwner(Owner owner);
-    public Owner updateOwnerImage();
-    public Owner updateOwnerPassword();
+    public Owner updateOwnerImage(Long id, MultipartFile image);
+    public Owner updateOwnerPassword(UpdatePasswordForm form, Long id);
 
     //Delete operation
     public void deleteOwner(Long id); 
