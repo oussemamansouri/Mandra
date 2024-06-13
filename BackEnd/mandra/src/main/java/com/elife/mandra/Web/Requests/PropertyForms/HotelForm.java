c3313bc05dd8568430d4b6c284d2c41eae7a6ff8
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-13 23:38:24 +0100

    Update HotelForm.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/Requests/PropertyForms/HotelForm.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/Requests/PropertyForms/HotelForm.java
@@ -7,7 +7,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class HotelForm {
 
     @NotBlank(message = "Le nom est obligatoire")
