73a407bf2088fd62dac6e99a6094a6d5f3659041
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-13 22:53:34 +0100

    Create HotelServiceImp.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/HotelServiceImp.java
@@ -0,0 +1,14 @@
+package com.elife.mandra.Business.ServicesImp;
+
+import com.elife.mandra.Business.Services.HotelService;
+import com.elife.mandra.DAO.Entities.Hotel;
+import com.elife.mandra.Web.Requests.PropertyForms.HotelForm;
+
+public class HotelServiceImp implements HotelService{
+
+    @Override
+    public Hotel addHotel(Long ownerId, HotelForm hotel) {
+        throw new UnsupportedOperationException("Unimplemented method 'addHotel'");
+    }
+
+}
