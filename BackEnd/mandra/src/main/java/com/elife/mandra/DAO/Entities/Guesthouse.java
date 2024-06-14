acc9c0f06091b4883afcbc985b10bd82cb66bb63
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-14 14:28:49 +0100

    Update Guesthouse.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Guesthouse.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Guesthouse.java
@@ -1,5 +1,8 @@
 package com.elife.mandra.DAO.Entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,14 +28,17 @@ public class Guesthouse extends Property {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "hasPool",nullable = false)
-    private boolean hasPool ;
-    
-    @Column(name = "hasRestaurant",nullable = false)
-    private boolean hasRestaurant ;
+    @Column(name = "hasPool", nullable = false)
+    private boolean hasPool;
+
+    @Column(name = "hasRestaurant", nullable = false)
+    private boolean hasRestaurant;
 
     @ManyToOne()
     @JoinColumn(name = "OwnerId")
-    private Owner owner ;
+    private Owner owner;
+
+    @OneToMany(mappedBy = "guesthouse", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GuesthouseImage> guestHouseImage;
 
 }
