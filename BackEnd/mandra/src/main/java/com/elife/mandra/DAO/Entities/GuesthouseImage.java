17a48fe693a4ac111b52960b21d5587b8f99b946
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-14 14:28:28 +0100

    Create GuesthouseImage.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/GuesthouseImage.java
@@ -0,0 +1,38 @@
+package com.elife.mandra.DAO.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "GuesthouseImages")
+public class GuesthouseImage {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le chemin de l'image est obligatoire")
+    @Column(name = "imagePath", nullable = false)
+    private String imagePath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "guestHouse_id", nullable = false)
+    private Guesthouse guesthouse;
+
+
+}
