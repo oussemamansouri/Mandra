4197b5655df4890a0829a3d64d7871086bb7b1e5
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-14 23:33:32 +0100

    Update HotelService.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/HotelService.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/HotelService.java
@@ -1,12 +1,17 @@
 package com.elife.mandra.Business.Services;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.elife.mandra.DAO.Entities.Hotel;
 import com.elife.mandra.Web.Requests.PropertyForms.HotelForm;
+
 
 public interface HotelService {
 
     //Create operation
-    public Hotel addHotel(Long ownerId, HotelForm hotel);
+    public Hotel addHotel(Long ownerId, HotelForm hotel, List<MultipartFile> images);
 
 
 }
