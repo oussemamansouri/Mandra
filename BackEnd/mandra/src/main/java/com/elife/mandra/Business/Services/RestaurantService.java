f79a4247d76695e988194e72428893ce42ec808f
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-15 19:47:42 +0100

    Create RestaurantService.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/RestaurantService.java
@@ -0,0 +1,15 @@
+package com.elife.mandra.Business.Services;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.elife.mandra.DAO.Entities.Restaurant;
+import com.elife.mandra.Web.Requests.PropertyForms.RestaurantForm;
+
+public interface RestaurantService {
+
+    //Create operation
+    public Restaurant addReataurant(Long ownerId, RestaurantForm restaurantForm, List<MultipartFile> images);
+
+}
