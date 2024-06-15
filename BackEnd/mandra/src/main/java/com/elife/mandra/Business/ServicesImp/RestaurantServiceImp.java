92c0f4304546e54fa0c166228b2ad7ae3ddcebe7
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-15 22:25:38 +0100

    Update RestaurantServiceImp.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/RestaurantServiceImp.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/RestaurantServiceImp.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -169,6 +171,21 @@ public class RestaurantServiceImp implements RestaurantService{
         } catch (Exception e) {
             LOGGER.error("Error while updating restaurant images", e);
             throw new RuntimeException("Error while updating restaurant images: " + e.getMessage(), e);
+        }
+    }
+
+
+
+
+    // ---------------------------------- get restaurants -----------------------------------
+
+    @Override
+    public Page<Restaurant> getRestaurants(Pageable pageable) {
+        try {
+            return restaurantRepository.findAll(pageable);
+        } catch (Exception e) {
+            LOGGER.error("Error while finding restaurants", e);
+            throw new RuntimeException("Failed to find restaurants: " + e.getMessage(), e);
         }
     }
     
