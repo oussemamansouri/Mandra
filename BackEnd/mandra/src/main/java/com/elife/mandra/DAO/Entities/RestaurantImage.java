c7dc062d84585469702dcf2ac2f353d480bbde65
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-15 19:47:29 +0100

    Update RestaurantImage.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/RestaurantImage.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/RestaurantImage.java
@@ -1,5 +1,7 @@
 package com.elife.mandra.DAO.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,9 +14,11 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 
@@ -31,6 +35,7 @@ public class RestaurantImage {
     private String imagePath;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
     @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
 
