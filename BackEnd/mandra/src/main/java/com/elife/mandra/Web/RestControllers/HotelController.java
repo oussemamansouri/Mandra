e6a69c08cf04eb65c0c37426bbcda57c705c8b30
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-15 17:54:54 +0100

    Update HotelController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/HotelController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/HotelController.java
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.elife.mandra.Business.Services.HotelService;
 import com.elife.mandra.DAO.Entities.Hotel;
 import com.elife.mandra.Web.Requests.PropertyForms.HotelForm;
+import com.elife.mandra.Web.Responses.ErrorResponse;
 
 import jakarta.validation.Valid;
 
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 
 @RestController
@@ -76,6 +78,21 @@ public class HotelController {
 }
 
 
+
+
+ // ----------------------------------      update Hotel images endpoint     -----------------------------------   
+
+ @PutMapping("/{hotelId}/edit-images")
+ public ResponseEntity<Object> updateHotelImages(@PathVariable(value = "hotelId") Long hotelId, 
+                                                 @RequestParam("images") List<MultipartFile> images) {
+     try {
+         Hotel updatedHotel = hotelService.updateHotelImages(hotelId, images);
+         return ResponseEntity.status(HttpStatus.ACCEPTED).body(updatedHotel);
+     } catch (RuntimeException e) {
+         ErrorResponse errorResponse = new ErrorResponse("Error while uploading hotel images", e.getMessage());
+         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+     }
+ }
 
 
 
