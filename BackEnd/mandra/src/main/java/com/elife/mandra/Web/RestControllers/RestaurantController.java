8dac2e4f3d369879f1f142136e6099aa301c6f1a
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-15 22:25:59 +0100

    get Restaurants endpoint works !!

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/RestaurantController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/RestaurantController.java
@@ -2,9 +2,13 @@ package com.elife.mandra.Web.RestControllers;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -90,4 +94,23 @@ public class RestaurantController {
          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
      }
  }
+
+
+
+     // ----------------------------------      get Restaurants endpoint     -----------------------------------
+
+ @GetMapping("")
+    public ResponseEntity<Object> getRestaurants(@PageableDefault(size = 10) Pageable pageable) {
+        try{
+       Page<Restaurant> restaurant = restaurantService.getRestaurants(pageable);
+       return ResponseEntity.status(HttpStatus.OK).body(restaurant);
+        }catch(Exception e){
+            ErrorResponse errorResponse = new ErrorResponse("Error while getting restaurants ", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+
+
+
 }
