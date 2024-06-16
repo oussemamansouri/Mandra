be6af924134a01a0c4cda2a92548065074b4dbc2
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-16 22:03:16 +0100

    Update GuesthouseService.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/GuesthouseService.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/GuesthouseService.java
@@ -2,6 +2,8 @@ package com.elife.mandra.Business.Services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.elife.mandra.DAO.Entities.Guesthouse;
@@ -15,5 +17,8 @@ public interface GuesthouseService {
     //Update operation
     public Guesthouse updateGuestHouse(Long guestHouseId, GuesthouseForm guesthouseForm);
     public Guesthouse updateGuestHouseImage(Long guestHouseId, List<MultipartFile> images);
+
+     //Read operation
+    public Page<Guesthouse> getGuestHouses(Pageable pageable);
 
 }
