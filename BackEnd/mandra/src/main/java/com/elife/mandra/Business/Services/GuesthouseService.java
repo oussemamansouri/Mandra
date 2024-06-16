0eb70ba1877f7551459fe9f556b1540b9ae3368e
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-16 21:08:05 +0100

    Create GuesthouseService.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/GuesthouseService.java
@@ -0,0 +1,15 @@
+package com.elife.mandra.Business.Services;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.elife.mandra.DAO.Entities.Guesthouse;
+import com.elife.mandra.Web.Requests.PropertyForms.GuesthouseForm;
+
+public interface GuesthouseService {
+
+    // Create operation
+    public Guesthouse addGuestHouse(Long ownerId, GuesthouseForm guesthouseForm, List<MultipartFile> images);
+
+}
