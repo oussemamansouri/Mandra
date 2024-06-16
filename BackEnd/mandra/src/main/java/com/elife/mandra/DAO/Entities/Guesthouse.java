1ebb8d5d55ca9f058db258228cd66059a3973b84
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-16 20:58:54 +0100

    Update Guesthouse.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Guesthouse.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Entities/Guesthouse.java
@@ -2,6 +2,8 @@ package com.elife.mandra.DAO.Entities;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -36,9 +38,29 @@ public class Guesthouse extends Property {
 
     @ManyToOne()
     @JoinColumn(name = "OwnerId")
+    @JsonIgnore
     private Owner owner;
 
     @OneToMany(mappedBy = "guesthouse", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<GuesthouseImage> guestHouseImage;
+
+    public Guesthouse(String name, String description, String email, String address, String city,
+            String phoneNumber, String website, boolean hasParking, boolean hasWifi, boolean allowsPets,
+            String facebook, String instagram, boolean hasPool, boolean hasRestaurant) {
+        this.setName(name);
+        this.setDescription(description);
+        this.setEmail(email);
+        this.setAddress(address);
+        this.setCity(city);
+        this.setPhoneNumber(phoneNumber);
+        this.setWebsite(website);
+        this.setHasParking(hasParking);
+        this.setHasWifi(hasWifi);
+        this.setAllowsPets(allowsPets);
+        this.setFacebook(facebook);
+        this.setInstagram(instagram);
+        this.hasPool = hasPool;
+        this.hasRestaurant = hasRestaurant;
+    }
 
 }
