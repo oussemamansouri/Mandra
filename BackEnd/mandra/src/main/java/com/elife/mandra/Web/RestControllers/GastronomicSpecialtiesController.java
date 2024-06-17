8ce5b91752ed5eb1d12e394b75840b6c9adb603d
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-17 19:29:09 +0100

    Get Gastronomic Specialties endpoint works !!

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/GastronomicSpecialtiesController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/GastronomicSpecialtiesController.java
@@ -10,10 +10,13 @@ import com.elife.mandra.Web.Responses.ErrorResponse;
 
 import jakarta.validation.Valid;
 
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -95,6 +98,22 @@ public class GastronomicSpecialtiesController {
             return ResponseEntity.status(HttpStatus.ACCEPTED).body(updatedgGastronomicSpecialtie);
         } catch (RuntimeException e) {
             ErrorResponse errorResponse = new ErrorResponse("Error while updating gastronomic specialtie image", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+
+    
+
+    // ---------------------------------- get Gastronomic Specialties endpoint -----------------------------------
+
+ @GetMapping("")
+    public ResponseEntity<Object> getGastronomicSpecialties(@PageableDefault(size = 10) Pageable pageable) {
+        try{
+       Page<GastronomicSpecialties> gastronomicSpecialties = gastronomicSpecialtiesService.getGastronomicSpecialties(pageable);
+       return ResponseEntity.status(HttpStatus.OK).body(gastronomicSpecialties);
+        }catch(Exception e){
+            ErrorResponse errorResponse = new ErrorResponse("Error while getting gastronomic specialties ", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
         }
     }
