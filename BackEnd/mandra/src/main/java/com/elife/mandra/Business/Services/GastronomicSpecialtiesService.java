ddfaebc4b88e1749a417f82cd2871ce7c2a7d7f8
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-17 18:48:14 +0100

    Update GastronomicSpecialtiesService.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/GastronomicSpecialtiesService.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/GastronomicSpecialtiesService.java
@@ -1,11 +1,14 @@
 package com.elife.mandra.Business.Services;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.elife.mandra.DAO.Entities.GastronomicSpecialties;
 import com.elife.mandra.Web.Requests.GastronomicSpecialtiesForm;
 
 public interface GastronomicSpecialtiesService {
 
     // Create operation
-    public GastronomicSpecialties addGastronomicSpecialties(Long adminId, GastronomicSpecialtiesForm gastronomicSpecialtiesForm);
+    public GastronomicSpecialties addGastronomicSpecialties(Long adminId, GastronomicSpecialtiesForm gastronomicSpecialtiesForm,
+    MultipartFile image );
 
 }
