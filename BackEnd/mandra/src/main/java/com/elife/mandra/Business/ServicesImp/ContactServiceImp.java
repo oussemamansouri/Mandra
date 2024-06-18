2fedacd8b9f3c90d30d8b1e218f79594b313a2b7
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-18 16:17:21 +0100

    Create ContactServiceImp.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/ContactServiceImp.java
@@ -0,0 +1,50 @@
+package com.elife.mandra.Business.ServicesImp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.elife.mandra.Business.Services.ContactService;
+import com.elife.mandra.DAO.Entities.Contact;
+import com.elife.mandra.DAO.Repositories.ContactRepository;
+import com.elife.mandra.Web.Requests.ContactForms.ContactForm;
+
+
+@Service
+public class ContactServiceImp implements ContactService{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContactServiceImp.class);
+
+    final ContactRepository contactRepository;
+    private ContactServiceImp(ContactRepository contactRepository){
+        this.contactRepository = contactRepository;
+    }
+
+
+
+
+
+    // ---------------------------------- add Contact By Visiter -----------------------------------
+
+    @Override
+    public Contact addContact(ContactForm contactForm) {
+         try {
+        
+            Contact newContact = new Contact(
+                contactForm.getFirstname(),
+                contactForm.getLastname(),
+                contactForm.getEmail(),
+                contactForm.getPhoneNumber(),
+                contactForm.getSubject(),
+                contactForm.getDescription()
+            );
+
+            return contactRepository.save(newContact);
+
+        } catch (Exception e) {
+            LOGGER.error("Error while adding contact", e);
+            throw new RuntimeException("Error while adding contact: " + e.getMessage(), e);
+        }
+    }
+
+}
