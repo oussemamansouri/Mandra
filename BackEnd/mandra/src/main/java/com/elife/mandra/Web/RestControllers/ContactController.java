0024e58b58445c6b2aa0507e62e8a5893af7b7b6
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-19 00:42:05 +0100

    Get Contacts endpoint works !!

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ContactController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ContactController.java
@@ -1,8 +1,12 @@
 package com.elife.mandra.Web.RestControllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.elife.mandra.Business.Services.ContactService;
 import com.elife.mandra.DAO.Entities.Contact;
 import com.elife.mandra.Web.Requests.ContactForms.ContactForm;
+import com.elife.mandra.Web.Responses.ErrorResponse;
 
 import jakarta.validation.Valid;
 
@@ -50,5 +55,19 @@ public class ContactController {
 
 
 
+
+
+    // ---------------------------------- get Contacts endpoint -----------------------------------
+
+ @GetMapping("")
+    public ResponseEntity<Object> getContacts(@PageableDefault(size = 10) Pageable pageable) {
+        try{
+       Page<Contact> contactPage = contactService.getContacts(pageable);
+       return ResponseEntity.status(HttpStatus.OK).body(contactPage);
+        }catch(Exception e){
+            ErrorResponse errorResponse = new ErrorResponse("Error while getting contacts ", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
 
 }
