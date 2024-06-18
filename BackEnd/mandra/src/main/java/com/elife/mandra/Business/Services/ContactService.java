ab3444bb84c9c64d31f222dd1d6b2e9ac88f9bb8
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-19 00:41:48 +0100

    Update ContactService.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/ContactService.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/ContactService.java
@@ -1,5 +1,8 @@
 package com.elife.mandra.Business.Services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.elife.mandra.DAO.Entities.Contact;
 import com.elife.mandra.Web.Requests.ContactForms.ContactForm;
 
@@ -7,5 +10,8 @@ public interface ContactService {
 
     //Create Operation
     public Contact addContact(ContactForm contactForm);
+
+    //Read Operation
+    public Page<Contact> getContacts(Pageable pageable);
 
 }
