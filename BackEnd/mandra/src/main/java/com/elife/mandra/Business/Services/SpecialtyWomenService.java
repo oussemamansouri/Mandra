fb5ab9d7dc90d8fd3e6981bcdb31d9865a96033a
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-18 01:46:21 +0100

    Create SpecialtyWomenService.java

---
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/Services/SpecialtyWomenService.java
@@ -0,0 +1,14 @@
+package com.elife.mandra.Business.Services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.elife.mandra.DAO.Entities.SpecialtyWomen;
+import com.elife.mandra.Web.Requests.SpecialtyWomenForms.SpecialtyWomenForm;
+
+public interface SpecialtyWomenService {
+
+    // Create Operation
+    public SpecialtyWomen addSpecialtyWomenForm(Long adminId, SpecialtyWomenForm specialtyWomenForm,
+            MultipartFile image);
+
+}
