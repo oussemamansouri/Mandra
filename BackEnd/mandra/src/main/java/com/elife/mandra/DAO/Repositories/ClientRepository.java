3adbcd9c294e3a35af0973909798a33b52aa3ffd
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-19 01:33:03 +0100

    Update ClientRepository.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/ClientRepository.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/ClientRepository.java
@@ -1,15 +1,20 @@
 package com.elife.mandra.DAO.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.elife.mandra.DAO.Entities.Client;
+import com.elife.mandra.DAO.Entities.OptionControl.AccountStateOption;
+
 import java.util.List;
 
-
 @Repository
-public interface ClientRepository extends JpaRepository<Client,Long> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
     public List<Client> findByEmail(String email);
+
+    public Page<Client> findByAccountState(AccountStateOption accountState, Pageable pageable);
 
 }
