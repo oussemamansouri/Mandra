5ec0c99166ba21f8d98cb30c27ab509c08fed512
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-21 00:31:08 +0100

    Update admin-layoute.component.ts

--- FrontEnd/mandra/src/app/layoutes/admin-layoute/admin-layoute.component.ts
+++ FrontEnd/mandra/src/app/layoutes/admin-layoute/admin-layoute.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'app-admin-layoute',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-layoute.component.css']
 })
 export class AdminLayouteComponent {
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+
+  }
+
+  sidebarToggle()
+  {
+    //toggle sidebar function
+    this.document.body.classList.toggle('toggle-sidebar');
+  }
+
 
 }
