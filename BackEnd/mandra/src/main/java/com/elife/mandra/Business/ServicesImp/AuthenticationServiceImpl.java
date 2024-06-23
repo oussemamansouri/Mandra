aecb79fba25d69d7076bb7aa7793ff55af54e615
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-24 00:03:33 +0100

    Update AuthenticationServiceImpl.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/AuthenticationServiceImpl.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Business/ServicesImp/AuthenticationServiceImpl.java
@@ -2,21 +2,30 @@ package com.elife.mandra.Business.ServicesImp;
 
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.elife.mandra.Business.Services.AuthenticationService;
 import com.elife.mandra.DAO.Entities.Client;
+import com.elife.mandra.DAO.Entities.Admin;
+import com.elife.mandra.DAO.Entities.Owner;
 import com.elife.mandra.Web.Requests.AuthenticationDTO.AuthenticationUserDTO;
 
 @Service
-public class AuthenticationServiceImpl implements AuthenticationService{
-
+public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public AuthenticationUserDTO login(Authentication authentication) {
-        // Retrieve the user principal from the authentication object after basic authentication
-        Client client = (Client) authentication.getPrincipal();
-        // Convert the User entity to AuthenticationUserDTO and return it
-        return AuthenticationUserDTO.toAuthenticationUserDTO(client);
-    }
+        // Retrieve the user principal from the authentication object
+        UserDetails user = (UserDetails) authentication.getPrincipal();
 
+        if (user instanceof Client) {
+            return AuthenticationUserDTO.toAuthenticationUserDTO((Client) user);
+        } else if (user instanceof Admin) {
+            return AuthenticationUserDTO.toAuthenticationUserDTO((Admin) user);
+        } else if (user instanceof Owner) {
+            return AuthenticationUserDTO.toAuthenticationUserDTO((Owner) user);
+        } else {
+            throw new IllegalArgumentException("Unsupported user type");
+        }
+    }
 }
