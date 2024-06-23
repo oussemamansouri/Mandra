e4e74bc0b09304984c39a73e9b046bcb055bfd9a
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-23 02:30:02 +0100

    Update SecurityConfiguration.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/config/SecurityConfiguration.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/config/SecurityConfiguration.java
@@ -1,15 +1,80 @@
 package com.elife.mandra.config;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import com.elife.mandra.DAO.Repositories.ClientRepository;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 public class SecurityConfiguration {
+
+      //The RSA keys used for encoding and decoding JWTs.
+    private final RSAPublicKey key;
+    private final RSAPrivateKey priv;
+    private final ClientRepository clientRepository;
+    
+    public SecurityConfiguration(@Value("${jwt.public.key}") RSAPublicKey key,
+                                     @Value("${jwt.private.key}") RSAPrivateKey priv,
+                                     ClientRepository clientRepository) {
+        this.key = key;
+        this.priv = priv;
+        this.clientRepository = clientRepository;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+     // Bean for loading client details from the database
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> clientRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Client not found"));
+    }
+
+
+    // Bean for JWT decoder using the public key
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.key).build();
+    }
+
+    // Bean for JWT encoder using the public and private keys
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.key).privateKey(this.priv).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+   // Bean for JwtAuthenticationConverter
+   @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        // Remove the SCOPE_ prefix
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+       
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
 }
