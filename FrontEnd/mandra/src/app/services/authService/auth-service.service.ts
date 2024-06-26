f275a5addb8e6ec9251e006b7652805517e70782
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-26 23:51:38 +0100

    Update auth-service.service.ts

--- FrontEnd/mandra/src/app/services/authService/auth-service.service.ts
+++ FrontEnd/mandra/src/app/services/authService/auth-service.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, // Inject HttpClient for HTTP requests
+    private router: Router, // Inject Router for navigation
+    @Inject('BaseURL') private baseURL: any, // Inject baseURL for API endpoints
+  ) { }
 }
