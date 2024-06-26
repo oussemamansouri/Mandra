ea4af1a59058ff69a11669ddaa7d5a28879c5755
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-27 00:56:09 +0100

    Update app-routing.module.ts

--- FrontEnd/mandra/src/app/app-routing.module.ts
+++ FrontEnd/mandra/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayouteComponent } from './layoutes/admin-layoute/admin-layoute.component';
+import { authenticationGuard } from './guards/authentication.guard';
 
 const routes: Routes = [
-  {path:'admin',component:AdminLayouteComponent,children:[
+  {path:'admin',component:AdminLayouteComponent,canActivate:[authenticationGuard],data: {roles: ['Admin']},children:[
     {path:'',redirectTo:'dashboard',pathMatch:'full'},
     {path:'dashboard',loadChildren:()=> import('./views/Admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
     {path:'profile',loadChildren:()=> import('./views/Admin/profile/profile.module').then(m=>m.ProfileModule)},
