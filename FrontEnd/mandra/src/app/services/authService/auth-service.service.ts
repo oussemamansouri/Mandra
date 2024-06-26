7b24b463647d219ae173c5aa2a717532be9d9bf3
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-26 23:58:49 +0100

    Update auth-service.service.ts

--- FrontEnd/mandra/src/app/services/authService/auth-service.service.ts
+++ FrontEnd/mandra/src/app/services/authService/auth-service.service.ts
@@ -1,11 +1,15 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { User } from 'src/app/Shared/user';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
+
+  AuthenticatedUser$ = new BehaviorSubject<User | null>(null); // BehaviorSubject to hold the authenticated user
 
   constructor(
     private http: HttpClient, // Inject HttpClient for HTTP requests
