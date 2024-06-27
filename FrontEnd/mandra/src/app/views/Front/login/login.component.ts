f3d75cdb6f80fe7d701f58d92d2610d1a01f1bf6
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-27 15:08:05 +0100

    update login

--- FrontEnd/mandra/src/app/views/Front/login/login.component.ts
+++ FrontEnd/mandra/src/app/views/Front/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/services/authService/auth-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,59 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  token:any
+  errmessage:any
+    data:any
+    // url:any
+    // helper=new JwtHelperService
+    // constructor(private auth:AuthServiceService,private route:Router,private act:ActivatedRoute) { }
 
+    ngOnInit(): void {
+      // this.url=this.act.snapshot.queryParams['backurl']
+      // if(this.auth.logedin()==true){
+      //   this.token=localStorage.getItem('token')
+      //   let decodeToken = this.helper.decodeToken(this.token)
+      //   switch(decodeToken.role) {
+      //     case 'admin':
+      //       this.route.navigate(['/admin'])
+      //       break;
+      //     case 'client':
+      //       this.route.navigate(['/client'])
+      //       break;
+      //       case 'centre':
+      //         this.route.navigate(['/centre'])
+      //         break;
+      //     default:
+      //       this.route.navigate(['/login'])
+      //   }
+      // }
+    }
 
+    loginuser(f:any){
+    //   let body=f.value
+    //   this.auth.login(body).subscribe(res=>{
+    //     this.data=res
+    //     this.auth.savedata(this.data.token.token)
+    //     let decodeToken = this.helper.decodeToken(this.data.token.token)
+    //     // if (this.url){this.route.navigate([this.url])}
 
+    //     switch(decodeToken.role) {
+    //       case 'admin':
+    //         this.route.navigate(['/admin'])
+    //         break;
+    //       case 'client':
+    //         this.route.navigate(['/client'])
+    //         break;
+    //         case 'centre':
+    //           this.route.navigate(['/centre'])
+    //           break;
+    //       default:
+    //         this.route.navigate(['/login'])
+    //     }
 
+    //   },(err:HttpErrorResponse)=>this.errmessage=err.error)
+    // }
 
-  //  // Lifecycle hook that is called when the component is destroyed
-  //  ngOnDestroy() {
-  //   // Unsubscribe from the AuthenticatedUser$ observable to prevent memory leaks
-  //   this.AuthUserSub.unsubscribe();
-  // }
+}
 
 }
