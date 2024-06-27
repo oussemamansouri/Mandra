14e51004b267d47a27519805462d1ab546929c68
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-27 15:07:21 +0100

    Update the front layoute

--- FrontEnd/mandra/src/app/layoutes/front-layoute/front-layoute.component.ts
+++ FrontEnd/mandra/src/app/layoutes/front-layoute/front-layoute.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-front-layoute',
@@ -6,5 +8,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./front-layoute.component.css']
 })
 export class FrontLayouteComponent {
+
+  isSidebarOpen = false;
+  data:any
+  imagepath:any='http://localhost:8080/'
+
+
+  constructor(@Inject(DOCUMENT) private document: Document,private route:Router) {}
+
+  ngOnInit(): void {
+    // const token = localStorage.getItem('token');
+    // if (token) {
+    //   const decodedToken = this.helper.decodeToken(token);
+    //   if (decodedToken.role === 'client' ) {
+    //     decodedToken.role = 'Apprenant';
+    //   }
+    //   this.data = decodedToken;
+    // }else{this.data=''}
+  }
+
+  sidebarToggle() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  logout(){
+    localStorage.removeItem('token')
+    this.route.navigate(['/'])
+    this.ngOnInit()
+       }
+
+  navigateprofile(){
+    switch(this.data.role) {
+      case 'admin':
+        this.route.navigate(['/admin'])
+        break;
+      case 'client':
+        this.route.navigate(['/client'])
+        break;
+        case 'centre':
+          this.route.navigate(['/centre'])
+          break;
+      default:
+        this.route.navigate(['/login'])
+    }
+
+  }
+
+
+
+  scrollTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+
+  }
 
 }
