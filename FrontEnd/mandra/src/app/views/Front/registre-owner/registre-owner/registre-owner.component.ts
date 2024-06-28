634e6fbd7ec6cfc533be59650c924e49fb705f60
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-28 02:14:13 +0100

    Update registre-owner.component.ts

--- FrontEnd/mandra/src/app/views/Front/registre-owner/registre-owner/registre-owner.component.ts
+++ FrontEnd/mandra/src/app/views/Front/registre-owner/registre-owner/registre-owner.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ApiServiceService } from 'src/app/services/apiService/api-service.service';
+import { OwnerService } from 'src/app/services/apiServices/OwnerService/owner.service';
 
 @Component({
   selector: 'app-registre-owner',
@@ -10,15 +10,18 @@ import { ApiServiceService } from 'src/app/services/apiService/api-service.servi
 })
 export class RegistreOwnerComponent {
 
-  errmessage: any
-  constructor(private api: ApiServiceService, private router: Router) { }
+  errormessage!: string
+  constructor(private ownerService: OwnerService, private router: Router) { }
 
   ngOnInit(): void {
   }
   registre(f: NgForm) {
-    // let body = f.value
-    // this.api.registrecentre(body).subscribe(info => this.router.navigate(['/login']),
-    //  (err: HttpErrorResponse) => this.errmessage = 'Ce email est déjà utilisé')
+    let owner = f.value;
+    this.ownerService.registerOwner(owner)
+      .subscribe(response => this.router.navigate(['/signin']),(err)=>{
+        console.log(err)
+        this.errormessage='Ce email est déjà utilisé'
+      });
   }
 
 }
