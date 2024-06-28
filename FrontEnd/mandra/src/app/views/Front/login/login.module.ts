bf41e9eddcbe3b2fc236e0d6b8274157b3a1f0f1
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-28 02:15:56 +0100

    Update login.module.ts

--- FrontEnd/mandra/src/app/views/Front/login/login.module.ts
+++ FrontEnd/mandra/src/app/views/Front/login/login.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
+import { AuthServiceService } from 'src/app/services/authService/auth-service.service';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     LoginRoutingModule,
     FormsModule
-  ]
+  ],
+  providers: [AuthServiceService],
 })
 export class LoginModule { }
