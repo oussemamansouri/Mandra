3e6e5488a0b1e3d4bd51701b45a86039fb4d381e
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-28 02:14:33 +0100

    Create process-httpmsg-service.spec.ts

---
+++ FrontEnd/mandra/src/app/Shared/process-httpmsg-service.spec.ts
@@ -0,0 +1,7 @@
+import { ProcessHttpmsgService } from './process-httpmsg-service';
+
+describe('ProcessHttpmsgService', () => {
+  it('should create an instance', () => {
+    expect(new ProcessHttpmsgService()).toBeTruthy();
+  });
+});
