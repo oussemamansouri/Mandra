2fc8231514070de7154302664df7db516dac9005
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-28 02:14:24 +0100

    Update registre-client.component.ts

--- FrontEnd/mandra/src/app/views/Front/registre-client/registre-client/registre-client.component.ts
+++ FrontEnd/mandra/src/app/views/Front/registre-client/registre-client/registre-client.component.ts
@@ -1,45 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ApiServiceService } from 'src/app/services/apiService/api-service.service';
+import { ClientService } from 'src/app/services/apiServices/ClientService/client.service';
 
 @Component({
   selector: 'app-registre-client',
   templateUrl: './registre-client.component.html',
   styleUrls: ['./registre-client.component.css']
 })
-export class RegistreClientComponent {
+export class RegistreClientComponent implements OnInit {
 
-  errormessage:any
-
-
-  constructor(private api: ApiServiceService, private router: Router) { }
+  errormessage!:string
+  constructor(private clinetService: ClientService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  maxDate() {
-    const today = new Date().toISOString().split('T')[0];
-    return today;
-}
-
 
 
   register(f: NgForm) {
-
-    let body=new FormData
-    body.append('firstname',f.value.firstname)
-    body.append('lastname',f.value.lastname)
-    body.append('email',f.value.email)
-    body.append('tel',f.value.tel)
-    body.append('address',f.value.address)
-    body.append('dob',f.value.dob)
-    body.append('password',f.value.password)
-    // this.api.registreclient(body)
-    //   .subscribe(response => this.router.navigate(['/login']),(err)=>{
-    //     this.errormessage='Ce email est déjà utilisé'
-    //   });
-
+    let client = f.value;
+    console.log(client)
+    this.clinetService.registerClient(client)
+      .subscribe(response => this.router.navigate(['/signin']),(err)=>{
+        console.log(err)
+        this.errormessage='Ce email est déjà utilisé'
+      });
   }
 
 }
