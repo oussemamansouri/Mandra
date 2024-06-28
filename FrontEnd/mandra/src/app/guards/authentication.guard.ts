9f3f0882dee330a9d307216f3b5a27f606538133
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-28 18:47:37 +0100

    Update authentication.guard.ts

--- FrontEnd/mandra/src/app/guards/authentication.guard.ts
+++ FrontEnd/mandra/src/app/guards/authentication.guard.ts
@@ -7,27 +7,29 @@ export const authenticationGuard: CanActivateFn = (route: ActivatedRouteSnapshot
   const router = inject(Router);
   const authService = inject(AuthServiceService);
 
-  console.log('Guard activated');
+  // console.log('Guard activated');
+
+  authService.autoLogin();
 
   return authService.AuthenticatedUser$.pipe(
     take(1), // take the first one and then unsubscribe automatically
     map(user => {
-      console.log('User from AuthService:', user);
+      // console.log('User from AuthService:', user);
       // check if route is restricted by role
       const roles = route.data['roles'] as string[];
-      console.log('Required roles:', roles);
+      // console.log('Required roles:', roles);
 
       if (user && user.role && roles.includes(user.role.name)) {
-        console.log('User has required role, allowing access');
+        // console.log('User has required role, allowing access');
         return true; // User has the required role
       }
 
       if (user) {
-        console.log('User authenticated but does not have required role, redirecting to /forbidden');
+        // console.log('User authenticated but does not have required role, redirecting to /forbidden');
         return router.createUrlTree(['/forbidden']); // User is authenticated but doesn't have the required role
       }
 
-      console.log('User not authenticated, redirecting to /signin');
+      // console.log('User not authenticated, redirecting to /signin');
       return router.createUrlTree(['/signin']); // User is not authenticated
     })
   );
