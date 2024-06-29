ca7e5587c6b898d7acff0b39e2b09c9084f5865c
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-29 02:07:55 +0100

    Create add owner componant

---
+++ FrontEnd/mandra/src/app/views/Admin/add-owner/add-owner/add-owner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-owner',
+  templateUrl: './add-owner.component.html',
+  styleUrls: ['./add-owner.component.css']
+})
+export class AddOwnerComponent {
+
+}
