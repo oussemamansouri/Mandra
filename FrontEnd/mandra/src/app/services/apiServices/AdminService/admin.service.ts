8a9ebd28a1e0f144654cd30cae8114d750152130
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-29 01:34:44 +0100

    Update admin.service.ts

--- FrontEnd/mandra/src/app/services/apiServices/AdminService/admin.service.ts
+++ FrontEnd/mandra/src/app/services/apiServices/AdminService/admin.service.ts
@@ -13,13 +13,33 @@ export class AdminService {
     withCredentials: true
   };
 
+  httpSimpleOptions = {
+    withCredentials: true
+  };
 
   constructor(private http: HttpClient, @Inject('BaseURL') private baseUrl: string,
     private processHTTPMsgService: ProcessHttpmsgService) { }
 
 
-    getAdmin(adminId: number): Observable<any> {
+  getAdmin(adminId: number): Observable<any> {
     return this.http.get<any>(this.baseUrl + `/admin/${adminId}`, this.httpOptions)
-    .pipe(catchError(this.processHTTPMsgService.handleError));
+      .pipe(catchError(this.processHTTPMsgService.handleError));
   }
+
+  updateAdminImage(adminId: number, formData: FormData): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/admin/${adminId}/edit-image`, formData, this.httpSimpleOptions)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+
+  updateAdmin(adminId: number, updatedAdmin: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/admin/${adminId}/edit`, updatedAdmin, this.httpOptions)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+
+
+  updateAdminPassword(adminId: number, updatedAdminPassword: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/admin/${adminId}/edit-password`, updatedAdminPassword, this.httpOptions)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+
 }
