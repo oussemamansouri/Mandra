8ff206c1a21681f0c8f216032fcf78ff177fd017
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-29 01:34:50 +0100

    Update admin-layoute.component.ts

--- FrontEnd/mandra/src/app/layoutes/admin-layoute/admin-layoute.component.ts
+++ FrontEnd/mandra/src/app/layoutes/admin-layoute/admin-layoute.component.ts
@@ -1,24 +1,60 @@
-import { Component, OnInit,Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common'
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/services/authService/auth-service.service';
+import { Subscription } from 'rxjs';
+import { AdminService } from 'src/app/services/apiServices/AdminService/admin.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-layoute',
   templateUrl: './admin-layoute.component.html',
   styleUrls: ['./admin-layoute.component.css']
 })
-export class AdminLayouteComponent {
+export class AdminLayouteComponent implements OnInit, OnDestroy {
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  user!: any;
+  AuthUserSub!: Subscription;
+  baseURL!: string;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+    private authService: AuthServiceService,
+    private adminService: AdminService,
+    @Inject('BaseURL') private BaseURL: string
+  ) {}
 
   ngOnInit(): void {
+    this.authService.autoLogin();
 
+    this.AuthUserSub = this.authService.AuthenticatedUser$.subscribe({
+      next: user => {
+        if (user) {
+          this.baseURL = this.BaseURL;
+          this.adminService.getAdmin(user.id).subscribe({
+            next: data => {
+              this.user = data;
+            },
+            error: (err: HttpErrorResponse) => {
+              this.router.navigate(['/']);
+            }
+          });
+        }
+      }
+    });
   }
 
-  sidebarToggle()
-  {
-    //toggle sidebar function
+  sidebarToggle() {
     this.document.body.classList.toggle('toggle-sidebar');
   }
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 
+  ngOnDestroy() {
+    this.AuthUserSub.unsubscribe();
+  }
 }
