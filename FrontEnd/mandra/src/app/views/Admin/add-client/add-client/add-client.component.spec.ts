4cfef0bf52a3fe7e79e861ab68ae61e1159d0a32
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-01 00:52:13 +0100

    Add client BY admin works !

---
+++ FrontEnd/mandra/src/app/views/Admin/add-client/add-client/add-client.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddClientComponent } from './add-client.component';
+
+describe('AddClientComponent', () => {
+  let component: AddClientComponent;
+  let fixture: ComponentFixture<AddClientComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddClientComponent]
+    });
+    fixture = TestBed.createComponent(AddClientComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
