278c09a4f2e0513bdc3a776faa9ac5f8ab394f6f
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-30 22:07:57 +0100

    Create guast house componant and module

---
+++ FrontEnd/mandra/src/app/views/Admin/guast-house/guast-house/guast-house.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guast-house',
+  templateUrl: './guast-house.component.html',
+  styleUrls: ['./guast-house.component.css']
+})
+export class GuastHouseComponent {
+
+}
