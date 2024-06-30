f29c7c2d4e482d0a632790414a81700b179a0bd9
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-30 18:09:09 +0100

    Update owner-details-routing.module.ts

--- FrontEnd/mandra/src/app/views/Admin/owner-details/owner-details-routing.module.ts
+++ FrontEnd/mandra/src/app/views/Admin/owner-details/owner-details-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OwnerDetailsComponent } from './owner-details/owner-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:OwnerDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
