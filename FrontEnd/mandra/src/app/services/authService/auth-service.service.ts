ac32c0bfaaf9350d378ae21b6ca070e893512465
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-30 17:59:00 +0100

    Update auth-service.service.ts

--- FrontEnd/mandra/src/app/services/authService/auth-service.service.ts
+++ FrontEnd/mandra/src/app/services/authService/auth-service.service.ts
@@ -1,40 +1,37 @@
-import { Inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { User } from 'src/app/Shared/user';
-import { BehaviorSubject, catchError, tap, throwError } from 'rxjs';
-import { AuthResponseData } from 'src/app/Shared/auth-response-data';
-import { StorageServiceService } from '../storageService/storage-service.service';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { BehaviorSubject, catchError, throwError, tap } from "rxjs";
+import { AuthResponseData } from "src/app/Shared/auth-response-data";
+import { User } from "src/app/Shared/user";
+import { StorageServiceService } from "../storageService/storage-service.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
-
-  AuthenticatedUser$ = new BehaviorSubject<User | null>(null); // BehaviorSubject to hold the authenticated user
+  AuthenticatedUser$ = new BehaviorSubject<User | null>(null);
 
   constructor(
-    private http: HttpClient, // Inject HttpClient for HTTP requests
-    private router: Router, // Inject Router for navigation
-    private storageService: StorageServiceService, // Inject StorageService for local storage operations
-    @Inject('BaseURL') private baseURL: any, // Inject baseURL for API endpoints
+    private http: HttpClient,
+    private router: Router,
+    private storageService: StorageServiceService,
+    @Inject('BaseURL') private baseURL: any,
   ) { }
 
-
-  // Method to log in a user with email and password
   login(email: string, password: string) {
     let httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json', // Set content type to JSON
-        'Authorization': 'Basic ' + window.btoa(email + ':' + password) // Add basic auth header
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + window.btoa(email + ':' + password)
       }),
-      withCredentials: true // Include credentials (cookies) in the request
+      withCredentials: true
     };
 
     return this.http.post<AuthResponseData>(this.baseURL + '/auth/signin', null, httpOptions).pipe(
       catchError(err => {
         let errorMessage = 'An unknown error occurred!';
-        errorMessage = 'L’adresse e-mail ou le mot de passe que vous avez saisi est invalide';
+        errorMessage = 'The email or password you entered is incorrect.';
         return throwError(() => new Error(errorMessage));
       }),
       tap(user => {
@@ -46,34 +43,24 @@ export class AuthServiceService {
             permissions: user.roles.filter(permission => !permission.includes('ROLE'))
           }
         };
-        this.storageService.saveUser(extractedUser); // Save user to local storage
-        this.AuthenticatedUser$.next(extractedUser); // Update BehaviorSubject with authenticated user
+        this.storageService.saveUser(extractedUser);
+        this.AuthenticatedUser$.next(extractedUser);
       })
-    ).toPromise(); // Convert Observable to Promise to ensure completion before returning
+    ).toPromise();
   }
 
-    // Method to automatically log in a user if they are already authenticated
-    autoLogin() {
-      const userData = this.storageService.getSavedUser();
-      if (!userData) {
-        return;
-      }
-      this.AuthenticatedUser$.next(userData); // Update BehaviorSubject with authenticated user
+  autoLogin() {
+    const userData = this.storageService.getSavedUser();
+    if (userData) {
+      this.AuthenticatedUser$.next(userData);
     }
+  }
 
-
-
-    // Method to log out the current user
-    logout() {
-      this.http.request('post', this.baseURL + '/auth/signout', {
-        withCredentials: true // Include credentials (cookies) in the request
-      }).subscribe({
-        next: () => {
-          this.storageService.clean(); // Clear local storage
-          this.AuthenticatedUser$.next(null); // Reset authenticated user
-          this.router.navigate(['/signin']); // Navigate to the sign-in page
-        }
-      });
-    }
-
+  logout() {
+    this.http.post(this.baseURL + '/auth/signout', {}, { withCredentials: true }).subscribe(() => {
+      this.storageService.clean();
+      this.AuthenticatedUser$.next(null);
+      this.router.navigate(['/signin']);
+    });
+  }
 }
