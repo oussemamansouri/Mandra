173ff71fadeb51a7ba811932db01fd47b1095fb1
oussemamansouri <oussemamansouri619@gmail.com>
2024-06-30 17:59:06 +0100

    Update disabled-owners.module.ts

--- FrontEnd/mandra/src/app/views/Admin/disabled-owners/disabled-owners.module.ts
+++ FrontEnd/mandra/src/app/views/Admin/disabled-owners/disabled-owners.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { DisabledOwnersRoutingModule } from './disabled-owners-routing.module';
 import { DisabledOwnersComponent } from './disabled-owners/disabled-owners.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     DisabledOwnersRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class DisabledOwnersModule { }
