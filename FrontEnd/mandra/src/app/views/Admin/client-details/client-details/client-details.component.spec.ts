163558209a25f6cadc48af7168a2ec5c2f3ad299
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-01 02:03:09 +0100

    Client details works !

---
+++ FrontEnd/mandra/src/app/views/Admin/client-details/client-details/client-details.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientDetailsComponent } from './client-details.component';
+
+describe('ClientDetailsComponent', () => {
+  let component: ClientDetailsComponent;
+  let fixture: ComponentFixture<ClientDetailsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ClientDetailsComponent]
+    });
+    fixture = TestBed.createComponent(ClientDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
