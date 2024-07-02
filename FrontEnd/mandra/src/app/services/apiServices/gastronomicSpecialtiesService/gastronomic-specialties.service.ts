6361749205a5fe1ce47427df67827384bb50d39f
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-02 03:20:55 +0100

    Create gastronomic-specialties.service.ts

---
+++ FrontEnd/mandra/src/app/services/apiServices/gastronomicSpecialtiesService/gastronomic-specialties.service.ts
@@ -0,0 +1,37 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { ProcessHttpmsgService } from 'src/app/Shared/process-httpmsg-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GastronomicSpecialtiesService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    withCredentials: true
+  };
+
+  constructor(private http: HttpClient, @Inject('BaseURL') private baseUrl: string,
+    private processHTTPMsgService: ProcessHttpmsgService) { }
+
+
+    getGastronomicSpecialties(page: number, size: number): Observable<any> {
+      const url = `${this.baseUrl}/gastronomicspecialties?page=${page}&size=${size}`;
+      return this.http.get<any>(url, this.httpOptions)
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+
+
+    getGastronomicSpecialtieById(gastronomicspecialtieId: number): Observable<any> {
+      return this.http.get<any>(this.baseUrl + `/gastronomicspecialties/${gastronomicspecialtieId}`, this.httpOptions)
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+
+
+    deleteGastronomicSpecialtie(gastronomicspecialtieId: number): Observable<any> {
+      return this.http.delete<any>(`${this.baseUrl}/gastronomicspecialties/${gastronomicspecialtieId}/delete`, {  withCredentials: true})
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+
+}
