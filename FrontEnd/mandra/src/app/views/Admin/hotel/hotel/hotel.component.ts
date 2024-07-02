7d5f6d625f3093115fca02acc2999ce201d05352
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-02 20:47:55 +0100

    Adding spinner to hotels

--- FrontEnd/mandra/src/app/views/Admin/hotel/hotel/hotel.component.ts
+++ FrontEnd/mandra/src/app/views/Admin/hotel/hotel/hotel.component.ts
@@ -10,20 +10,19 @@ import { HotelService } from 'src/app/services/apiServices/hotelService/hotel.se
 })
 export class HotelComponent implements OnInit {
 
-
-  hotels!:any[]
-  hotel:any = {}
+  hotels: any[] = [];
+  hotel: any = {};
   page: number = 0;
   size: number = 12;
   totalPages: number = 0; // Initialize as 0
   baseURL!: string;
+  loading: boolean = false;
 
-
-  constructor( private hotelService:HotelService, private router:Router, @Inject("BaseURL") private BaseURL: string) {
+  constructor(private hotelService: HotelService, private router: Router, @Inject("BaseURL") private BaseURL: string) {
     this.baseURL = BaseURL;
-   }
+  }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.loadHotels();
   }
 
@@ -34,17 +33,19 @@ export class HotelComponent implements OnInit {
     }
   }
 
-
-  loadHotels(){
+  loadHotels(): void {
+    this.loading = true;
     this.hotelService.getHotels(this.page, this.size).subscribe({
-      next: (info:any) => {
+      next: (info: any) => {
         this.hotels = info.content || [];
         this.totalPages = info.totalPages || 0;
+        this.loading = false;
       },
-      error:(err: HttpErrorResponse) => {
+      error: (err: HttpErrorResponse) => {
         console.error('Error loading hotels:', err.message);
+        this.loading = false; 
       }
-  });
+    });
   }
 
   openModal(hotel: any): void {
@@ -57,15 +58,8 @@ export class HotelComponent implements OnInit {
 
   deleteHotel(): void {
     this.hotelService.deleteHotel(this.hotel.id).subscribe(
-      (res) =>
-        this.loadHotels()
-    ),(err:HttpErrorResponse)=>
-       console.log("error while deletting hotel")
+      (res) => this.loadHotels(),
+      (err: HttpErrorResponse) => console.error("Error while deleting hotel:", err.message)
+    );
   }
-
-
-  
-
-
-
 }
