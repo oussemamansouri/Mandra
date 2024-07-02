9ca51e9fb4544273b2489845a62350f9d924845a
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-02 18:15:20 +0100

    Update AdminController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/AdminController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.elife.mandra.Business.Services.AdminService;
 import com.elife.mandra.DAO.Entities.Admin;
-import com.elife.mandra.Web.Requests.UserForms.AddUserForm;
 import com.elife.mandra.Web.Requests.UserForms.UpdatePasswordForm;
 import com.elife.mandra.Web.Requests.UserForms.UpdateUserForm;
 import com.elife.mandra.Web.Responses.ErrorResponse;
@@ -31,26 +29,6 @@ public class AdminController {
     public AdminController(AdminService adminService){
         this.adminService = adminService;
     }   
-    
-
-// ----------------------------------      add admin endpoint     -----------------------------------
-
-@PostMapping("/add")
-    public ResponseEntity<?> addingAdmin(@Valid @RequestBody AddUserForm admin, BindingResult result) {
-        try{
-        if (result.hasErrors()) {
-            StringBuilder errors = new StringBuilder();
-            result.getAllErrors().forEach(error -> errors.append(error.getDefaultMessage()).append("; "));
-            return ResponseEntity.badRequest().body(errors.toString());
-        }
-        Admin addedClient = adminService.addAdmin(admin);
-        return ResponseEntity.status(HttpStatus.CREATED).body(addedClient);
-        }catch(Exception e){
-            ErrorResponse errorResponse = new ErrorResponse("Error while adding aadmin", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
-        }
-    }
-
 
 
 
