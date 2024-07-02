2d691461dd5ab1873c563ccd8921d1edee21017c
SawssenHasni <saousenhasni2020@gmail.com>
2024-07-02 14:42:06 +0200

    Update ReservationController.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ReservationController.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/Web/RestControllers/ReservationController.java
@@ -1,6 +1,8 @@
 package com.elife.mandra.Web.RestControllers;
 
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -40,6 +42,19 @@ public class ReservationController {
             return ResponseEntity.status(HttpStatus.CREATED).body(createdReservation);
         } catch (Exception e) {
             ErrorResponse errorResponse = new ErrorResponse("Error while creating reservation", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+    
+    // ---------------------------------- Get All Reservations endpoint -----------------------------------
+
+    @GetMapping("")
+    public ResponseEntity<Object> getAllReservations(@PageableDefault(size = 10) Pageable pageable) {
+        try {
+            Page<Reservation> reservationPage = reservationService.getAllReservations(pageable);
+            return ResponseEntity.status(HttpStatus.OK).body(reservationPage);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse("Error while getting all reservations", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
         }
     }
