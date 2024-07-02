fa691d29ac2c67b7bc18fb18c7f4791f7d0660e9
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-02 20:46:47 +0100

    Add spinner to disabled owners

--- FrontEnd/mandra/src/app/views/Admin/disabled-owners/disabled-owners/disabled-owners.component.ts
+++ FrontEnd/mandra/src/app/views/Admin/disabled-owners/disabled-owners/disabled-owners.component.ts
@@ -3,48 +3,49 @@ import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OwnerService } from 'src/app/services/apiServices/OwnerService/owner.service';
 
-
 @Component({
   selector: 'app-disabled-owners',
   templateUrl: './disabled-owners.component.html',
   styleUrls: ['./disabled-owners.component.css']
 })
-export class DisabledOwnersComponent implements OnInit{
+export class DisabledOwnersComponent implements OnInit {
   searchTerm: any;
   baseURL!: string;
-  owners: any[] = []; // Initialize as an empty array
+  owners: any[] = [];
   ownerid: any;
   page: number = 0;
   size: number = 10;
-  totalPages: number = 0; // Initialize as 0
+  totalPages: number = 0;
+  isLoading: boolean = true; // Add this line
 
-  constructor(@Inject("BaseURL") private BaseURL: string, private ownerService: OwnerService,
-  private changeDetectorRef: ChangeDetectorRef,
-   private router: Router) {
+  constructor(
+    @Inject("BaseURL") private BaseURL: string,
+    private ownerService: OwnerService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private router: Router
+  ) {
     this.baseURL = BaseURL;
   }
 
-
-
-
   ngOnInit(): void {
     this.loadOwners();
-    //  this.changeDetectorRef.detectChanges();
   }
 
   loadOwners(): void {
+    this.isLoading = true; 
     this.ownerService.getDisabledOwners(this.page, this.size).subscribe({
-      next: (info:any) => {
+      next: (info: any) => {
         this.owners = info.content || [];
         this.totalPages = info.totalPages || 0;
+        this.isLoading = false;
         this.changeDetectorRef.detectChanges();
       },
-      error:(err: HttpErrorResponse) => {
+      error: (err: HttpErrorResponse) => {
         console.error('Error loading owners:', err.message);
+        this.isLoading = false;
       }
-  });
+    });
   }
-
 
   onPageChange(newPage: number): void {
     if (newPage >= 0 && newPage < this.totalPages) {
@@ -53,69 +54,42 @@ export class DisabledOwnersComponent implements OnInit{
     }
   }
 
-
   getOwnerid(id: any) {
-    this.ownerid = id
+    this.ownerid = id;
   }
 
   deleteOwner(): void {
     if (this.ownerid) {
       this.ownerService.deleteOwner(this.ownerid).subscribe({
-        next:(res:any) => {
+        next: (res: any) => {
           console.log('Owner deleted successfully');
           this.loadOwners();
-          // this.changeDetectorRef.detectChanges();
         },
-        error:(err: HttpErrorResponse) => {
+        error: (err: HttpErrorResponse) => {
           console.error('Error while deleting owner:', err.message);
-          this.router.navigate(['/admin'])
+          this.router.navigate(['/admin']);
         }
-    });
+      });
     } else {
       console.error('No owner ID provided for deletion');
     }
   }
 
-
-
   changeToActive(id: number) {
     this.ownerService.changeOwnerStatus(id).subscribe({
-      next:() =>{
-        // this.loadOwners();
-        this.router.navigate(['/admin/owners/active'])
+      next: () => {
+        this.router.navigate(['/admin/owners/active']);
       },
-      error:(err:HttpErrorResponse) =>
+      error: (err: HttpErrorResponse) =>
         console.log("Error while changing owner account state")
-  })
+    });
   }
 
   sendId(id: any) {
-    this.router.navigate(['/admin/owners/details'], { queryParams: { ownerId: id } })
-
+    this.router.navigate(['/admin/owners/details'], { queryParams: { ownerId: id } });
   }
+
   sendOwnerId(id: any) {
-    this.router.navigate(['/admin/owners/update'], { queryParams: { ownerId: id } })
+    this.router.navigate(['/admin/owners/update'], { queryParams: { ownerId: id } });
   }
-
-
-
-
-  // filter() {
-  //   if (!this.searchTerm) {
-  //     return this.owners;
-  //   }
-
-  //   const filteredClients = this.owners.filter((client: any) => {
-  //     const firstNameMatch = client.firstname.toLowerCase().includes(this.searchTerm.toLowerCase());
-  //     const lastNameMatch = client.lastname.toLowerCase().includes(this.searchTerm.toLowerCase());
-
-  //     return firstNameMatch || lastNameMatch;
-  //   });
-
-  //   return filteredClients;
-  // }
-
-
-
-
 }
