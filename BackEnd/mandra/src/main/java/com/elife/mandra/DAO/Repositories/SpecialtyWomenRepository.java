2729b34bed78fe97e1f0a1372011526b0de38b0e
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-03 02:43:57 +0100

    Update SpecialtyWomenRepository.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/SpecialtyWomenRepository.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/SpecialtyWomenRepository.java
@@ -1,6 +1,10 @@
 package com.elife.mandra.DAO.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.elife.mandra.DAO.Entities.SpecialtyWomen;
@@ -8,5 +12,12 @@ import com.elife.mandra.DAO.Entities.SpecialtyWomen;
 
 @Repository
 public interface SpecialtyWomenRepository extends JpaRepository<SpecialtyWomen,Long> {
+
+     @Query("SELECT s FROM SpecialtyWomen s WHERE " +
+            "LOWER(s.specialty) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(s.description) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(s.city) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(s.address) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    Page<SpecialtyWomen> searchSpecialtyWomens(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 }
