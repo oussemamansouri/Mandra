88cca9cacc16d2a37acb6b2beb8f685993fd2ede
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-03 01:32:43 +0100

    Update HotelRepository.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/HotelRepository.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/HotelRepository.java
@@ -1,6 +1,10 @@
 package com.elife.mandra.DAO.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.elife.mandra.DAO.Entities.Hotel;
@@ -8,5 +12,12 @@ import com.elife.mandra.DAO.Entities.Hotel;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel,Long>{
+
+     @Query("SELECT h FROM Hotel h WHERE " +
+            "LOWER(h.name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(h.description) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(h.city) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(h.address) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    Page<Hotel> searchHotels(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 }
