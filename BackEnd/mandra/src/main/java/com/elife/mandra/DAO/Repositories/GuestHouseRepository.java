10b216581da074197bf451bcf61263ee150e1676
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-03 02:25:07 +0100

    Update GuestHouseRepository.java

--- BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/GuestHouseRepository.java
+++ BackEnd/mandra/src/main/java/com/elife/mandra/DAO/Repositories/GuestHouseRepository.java
@@ -1,6 +1,10 @@
 package com.elife.mandra.DAO.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.elife.mandra.DAO.Entities.Guesthouse;
@@ -8,5 +12,13 @@ import com.elife.mandra.DAO.Entities.Guesthouse;
 
 @Repository
 public interface GuestHouseRepository extends JpaRepository<Guesthouse,Long> {
+
+
+     @Query("SELECT g FROM Guesthouse g WHERE " +
+            "LOWER(g.name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(g.description) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(g.city) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+            "LOWER(g.address) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    Page<Guesthouse> searchGuestHouses(@Param("searchTerm") String searchTerm, Pageable pageable);
 
 }
