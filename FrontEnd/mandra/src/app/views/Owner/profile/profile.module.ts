63f7eec70049cc43932f6b5cb0937b3c7c7882b1
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-04 18:36:50 +0100

    Update profile.module.ts

--- FrontEnd/mandra/src/app/views/Owner/profile/profile.module.ts
+++ FrontEnd/mandra/src/app/views/Owner/profile/profile.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FormsModule
   ]
 })
 export class ProfileModule { }
